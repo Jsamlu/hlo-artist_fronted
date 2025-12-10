2dae07fe6cb64fba60da4668e37e946e84eeecbd
Jsamlu <jayeshsamlu69@gmail.com>
2025-12-10 18:23:31 +0530

    ty fix

--- src/pages/home/Masonary.jsx
+++ src/pages/home/Masonary.jsx
@@ -3,7 +3,7 @@ import { artWorkList } from "./artWorkList";
 import { Link } from "react-router-dom";
 const Masonary = () => {
   return (
-    <div className=" p-4 sm:columns-2xs columns-[150px]">
+    <div className=" p-4 md:columns-[300px] sm:columns-2xs columns-[100px]">
       {artWorkList.map((artwork) => {
         return (
           <Link to={`/${artwork.name}`}>
